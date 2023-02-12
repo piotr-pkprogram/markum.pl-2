586434856f4a7d3478282905fd23407ddc145a53
Piotr385 <74099800+Piotr385@users.noreply.github.com>
2023-02-12 22:02:52 +0100

    Add ecosystem.config.js file

---
+++ ecosystem.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  apps: [
+    {
+      name: 'NextAppName',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
+      env_local: {
+        APP_ENV: 'local',
+        'CONTENTFUL_API_PUBLIC_KEY': 'nrz0imrXtNkAmAhpoe6Y1nTV8cYAKZxyeQNs5hIYjiA',
+        'CONTENTFUL_API_PREVIEW_KEY': 'rjpkrg7XfxddBPD3v4Z-9z-8HT0nmrZozBGuhEvbrwo',
+        'CONTENTFUL_API_ID': 'xjfnd4eilgtd',
+        'CONTENTFUL_API_MANAGMENT_KEY': 'CFPAT-HKsmBrLGNUUSzdTheHNWnMWbeh-4kcc0TVZzwC00T7c',
+        'DOMAIN': 'http://134.122.92.71:3000',
+        'ESTICRM_ID': 6400,
+        'ESTICRM_TOKEN': '44b48b0294',
+      },
+      env_development: {
+        APP_ENV: 'dev',
+        'CONTENTFUL_API_PUBLIC_KEY': 'nrz0imrXtNkAmAhpoe6Y1nTV8cYAKZxyeQNs5hIYjiA',
+        'CONTENTFUL_API_PREVIEW_KEY': 'rjpkrg7XfxddBPD3v4Z-9z-8HT0nmrZozBGuhEvbrwo',
+        'CONTENTFUL_API_ID': 'xjfnd4eilgtd',
+        'CONTENTFUL_API_MANAGMENT_KEY': 'CFPAT-HKsmBrLGNUUSzdTheHNWnMWbeh-4kcc0TVZzwC00T7c',
+        'DOMAIN': 'http://134.122.92.71:3000',
+        'ESTICRM_ID': 6400,
+        'ESTICRM_TOKEN': '44b48b0294',
+      },
+      env_production: {
+        APP_ENV: 'prod',
+        'CONTENTFUL_API_PUBLIC_KEY': 'nrz0imrXtNkAmAhpoe6Y1nTV8cYAKZxyeQNs5hIYjiA',
+        'CONTENTFUL_API_PREVIEW_KEY': 'rjpkrg7XfxddBPD3v4Z-9z-8HT0nmrZozBGuhEvbrwo',
+        'CONTENTFUL_API_ID': 'xjfnd4eilgtd',
+        'CONTENTFUL_API_MANAGMENT_KEY': 'CFPAT-HKsmBrLGNUUSzdTheHNWnMWbeh-4kcc0TVZzwC00T7c',
+        'DOMAIN': 'http://marcinkumiszczo.pl',
+        'ESTICRM_ID': 6400,
+        'ESTICRM_TOKEN': '44b48b0294',
+      }
+    }
+  ]
+}

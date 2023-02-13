b3634f526a80fd44f727f34d0719ec7916711cc0
Piotr385 <74099800+Piotr385@users.noreply.github.com>
2023-02-13 14:34:20 +0100

    Upload new Offers

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: 'node_modules/next/dist/bin/next',
-      args: 'start',
+      args: 'dev',
       env_local: {
         APP_ENV: 'local',
         'CONTENTFUL_API_PUBLIC_KEY': 'nrz0imrXtNkAmAhpoe6Y1nTV8cYAKZxyeQNs5hIYjiA',
@@ -32,7 +32,7 @@ module.exports = {
         'CONTENTFUL_API_PREVIEW_KEY': 'rjpkrg7XfxddBPD3v4Z-9z-8HT0nmrZozBGuhEvbrwo',
         'CONTENTFUL_API_ID': 'xjfnd4eilgtd',
         'CONTENTFUL_API_MANAGMENT_KEY': 'CFPAT-HKsmBrLGNUUSzdTheHNWnMWbeh-4kcc0TVZzwC00T7c',
-        'DOMAIN': 'https://marcinkumiszczo.pl',
+        'DOMAIN': 'https://www.marcinkumiszczo.pl',
         'ESTICRM_ID': 6400,
         'ESTICRM_TOKEN': '44b48b0294',
       }

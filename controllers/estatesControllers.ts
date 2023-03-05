212cce240dc76e4fb2c7262715010bdc27d591b7
Piotr385 <74099800+Piotr385@users.noreply.github.com>
2023-03-05 18:56:05 +0100

    Fixed Typescript errors

--- controllers/estatesControllers.ts
+++ controllers/estatesControllers.ts
@@ -1,94 +1,102 @@
 import catchAsyncErrors from 'middlewares/catchAsyncErrors';
 import ErrorHandler from 'utils/errorHandler';
-import {uploadOffersFromEsti} from 'utils/uploadOffersFromEsti';
-import {NextApiRequest, NextApiResponse} from 'next';
-import {NextFunction} from 'connect';
-import {checkUploadTime, saveOffersToJSON} from 'utils/saveOffersToJSON';
+import { uploadOffersFromEsti } from 'utils/uploadOffersFromEsti';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { NextFunction } from 'connect';
+import { checkUploadTime, saveOffersToJSON } from 'utils/saveOffersToJSON';
 
 const uploadOffers = async () => {
-    const newEstates = Array.from(await uploadOffersFromEsti());
-    saveOffersToJSON(newEstates);
+  const newEstates = Array.from(await uploadOffersFromEsti());
+  saveOffersToJSON(newEstates);
 };
 
 export const getOffers = () => {
-    const isUploaded = checkUploadTime();
+  const isUploaded = checkUploadTime();
 
-    if (! isUploaded.checkTime && process.env.APP_ENV == 'dev') {
-        uploadOffers();
-    }
+  if (!isUploaded.checkTime && process.env.APP_ENV == 'dev') {
+    uploadOffers();
+  }
 
-    return isUploaded.estates;
+  return isUploaded.estates;
 };
 
-const getSingleEstateById = catchAsyncErrors(async (req : NextApiRequest, res : NextApiResponse, next : NextFunction) => {
-    const estateRes = Array.from(await getOffers()).filter((estate : any) => estate.id == req.query.id);
+const getSingleEstateById = catchAsyncErrors(
+  async (req: NextApiRequest, res: NextApiResponse, next: NextFunction) => {
+    const estateRes = Array.from(await getOffers()).filter(
+      (estate: any) => estate.id == req.query.id
+    );
     let estate;
-    if (! estateRes) {
-        return next(new ErrorHandler(404, 'Please enter correct id. Cannot find estate.'));
+    if (!estateRes) {
+      return next(new ErrorHandler(404, 'Please enter correct id. Cannot find estate.'));
     } else {
-        estate = estateRes[0];
+      estate = estateRes[0];
     }
 
-    res.status(200).json({success: true, estate});
-});
+    res.status(200).json({ success: true, estate });
+  }
+);
 
-const getSingleEstateByLink = catchAsyncErrors(async (req : NextApiRequest, res : NextApiResponse, next : NextFunction) => {
-    const estateRes = Array.from(await getOffers()).filter((estate : any) => estate.link == req.query.link);
+const getSingleEstateByLink = catchAsyncErrors(
+  async (req: NextApiRequest, res: NextApiResponse, next: NextFunction) => {
+    const estateRes = Array.from(await getOffers()).filter(
+      (estate: any) => estate.link == req.query.link
+    );
 
     let estate;
     if (!(estateRes.length > 0)) {
-        return next(new ErrorHandler(404, 'Please enter correct link. Cannot find estate.'));
+      return next(new ErrorHandler(404, 'Please enter correct link. Cannot find estate.'));
     } else {
-        estate = estateRes[0];
+      estate = estateRes[0];
     }
 
-    res.status(200).json({success: true, estate});
-});
+    res.status(200).json({ success: true, estate });
+  }
+);
 
-export const getAllEstates = catchAsyncErrors(async (req : NextApiRequest, res : NextApiResponse, next : NextFunction) => {
+export const getAllEstates = catchAsyncErrors(
+  async (req: NextApiRequest, res: NextApiResponse, next: NextFunction) => {
     try {
-        if (req.query.id && req.query.id !== '') {
-            getSingleEstateById(req, res, next);
-        } else if (req.query.link && req.query.link !== '') {
-            getSingleEstateByLink(req, res, next);
-        } else {
-            let resPerPage;
+      if (req.query.id && req.query.id !== '') {
+        getSingleEstateById(req, res, next);
+      } else if (req.query.link && req.query.link !== '') {
+        getSingleEstateByLink(req, res, next);
+      } else {
+        let resPerPage;
 
-            if ('isMore' in req.query && req.query.isMore !== 'false') 
-                resPerPage = 12;
-             else 
-                resPerPage = 8;
-            
+        if ('isMore' in req.query && req.query.isMore !== 'false') resPerPage = 12;
+        else resPerPage = 8;
 
-            let estates = Array.from(await getOffers());
-            let estatesCount = estates.length;
+        let estates = Array.from(await getOffers());
+        let estatesCount = estates.length;
 
-            if (req.query.page) {
-                const currentPage = Number(req.query.page) || 1;
-                const skip = resPerPage * (currentPage - 1);
-                let query;
+        if (req.query.page) {
+          const currentPage = Number(req.query.page) || 1;
+          const skip = resPerPage * (currentPage - 1);
+          let query;
 
-                if (req.query.category) {
-                    estatesCount = estates.filter((estate : any) => estate.category === req.query.category).length;
-                    query = estates.filter((_, index) => index >= skip);
-                    query = query.filter((estate : any) => estate.category === req.query.category);
-                    query = query.slice(0, resPerPage);
-                } else 
-                    query = estates.filter((estate, index) => index > skip);
-                 estates = query;
-            }
-            const filteredEstatesCount = estates.length;
-
-            res.status(200).json({
-                success: true,
-                estatesCount,
-                resPerPage,
-                filteredEstatesCount,
-                estates
-            });
+          if (req.query.category) {
+            estatesCount = estates.filter(
+              (estate: any) => estate.category === req.query.category
+            ).length;
+            query = estates.filter((_, index) => index >= skip);
+            query = query.filter((estate: any) => estate.category === req.query.category);
+            query = query.slice(0, resPerPage);
+          } else query = estates.filter((estate, index) => index > skip);
+          estates = query;
         }
+        const filteredEstatesCount = estates.length;
+
+        res.status(200).json({
+          success: true,
+          estatesCount,
+          resPerPage,
+          filteredEstatesCount,
+          estates
+        });
+      }
     } catch (err) {
-        console.log(err);
-        res.status(500).json({success: false, error: err});
+      console.log(err);
+      res.status(500).json({ success: false, error: err });
     }
-});
+  }
+);
--- utils/uploadOffersFromEsti.ts
+++ utils/uploadOffersFromEsti.ts
@@ -1,147 +1,167 @@
 // @ts-ignore
-import {EstiCRMType} from 'types/EstiCRMType';
+import { EstiCRMType } from 'types/EstiCRMType';
 import deletePolishChars from 'utils/deletePolishChars';
 import axios from 'axios';
-import {IEstateFields} from 'types/generated/contentful';
-import {promisify} from 'util';
+import { IEstateFields } from 'types/generated/contentful';
+import { promisify } from 'util';
 import https from 'https';
 import fs from 'fs';
 
 const sizeOf = promisify(require('image-size'));
 
-async function isImgUrl(url) {
-    return await fetch(url, {method: 'HEAD'}).then(res => {
-        return res.headers.get('Content-Type').startsWith('image')
-    })
+async function isImgUrl(url: string) {
+  return await fetch(url, { method: 'HEAD' }).then((res) => {
+    // @ts-ignore
+    return res.headers.get('Content-Type').startsWith('image');
+  });
 }
 
-export const uploadOffersFromEsti = async () : Promise < IEstateFields[] > => {
-    const estiOffers = await axios.get(`https://app.esticrm.pl/apiClient/offer/list?company=${
-        process.env.ESTICRM_ID
-    }&token=${
-        process.env.ESTICRM_TOKEN
-    }&take=1000`);
+export const uploadOffersFromEsti = async (): Promise<IEstateFields[]> => {
+  const estiOffers = await axios.get(
+    `https://app.esticrm.pl/apiClient/offer/list?company=${process.env.ESTICRM_ID}&token=${process.env.ESTICRM_TOKEN}&take=1000`
+  );
 
-    let offers = Array.isArray(estiOffers.data.data) ? estiOffers.data.data : [{
-            ... estiOffers.data.data
-        }];
-    offers = await Promise.all(offers.filter((offer : EstiCRMType) => offer.contactFirstname === 'Marcin' && offer.contactLastname === 'Kumiszczo').map(async (offer : EstiCRMType) => {
-        offer.locationStreetName = offer.locationStreetName ? offer.locationStreetName : offer.locationPrecinctName;
-        offer.locationPrecinctName = offer.locationParentPrecinctName ? offer.locationParentPrecinctName : offer.locationPrecinctName;
+  let offers = Array.isArray(estiOffers.data.data)
+    ? estiOffers.data.data
+    : [
+        {
+          ...estiOffers.data.data
+        }
+      ];
+  offers = await Promise.all(
+    offers
+      .filter(
+        (offer: EstiCRMType) =>
+          offer.contactFirstname === 'Marcin' && offer.contactLastname === 'Kumiszczo'
+      )
+      .map(async (offer: EstiCRMType) => {
+        offer.locationStreetName = offer.locationStreetName
+          ? offer.locationStreetName
+          : offer.locationPrecinctName;
+        offer.locationPrecinctName = offer.locationParentPrecinctName
+          ? offer.locationParentPrecinctName
+          : offer.locationPrecinctName;
 
         const link = `/${
-            offer.transaction && offer.transaction == 132 ? 'na-wynajem' : 'na-sprzedaz'
+          offer.transaction && offer.transaction == 132 ? 'na-wynajem' : 'na-sprzedaz'
         }/${
-            offer.locationStreetName ? deletePolishChars(offer.locationStreetName).toLowerCase() : offer.locationParentPrecinctName ? deletePolishChars(offer.locationParentPrecinctName).toLowerCase() : ''
+          offer.locationStreetName
+            ? deletePolishChars(offer.locationStreetName).toLowerCase()
+            : offer.locationParentPrecinctName
+            ? deletePolishChars(offer.locationParentPrecinctName).toLowerCase()
+            : ''
         }${
-            offer.locationBuildingnumber ? `-${
-                offer.locationBuildingnumber
-            }-` : '-'
-        }${
-            deletePolishChars(offer.locationCityName).toLowerCase()
-        }`;
+          offer.locationBuildingnumber ? `-${offer.locationBuildingnumber}-` : '-'
+        }${deletePolishChars(offer.locationCityName).toLowerCase()}`;
 
         const newOffer = {
-            id: parseInt(offer.id),
-            category: offer.transaction && offer.transaction == 132 ? 'FOR_RENT' : 'FOR_SALE',
-            link: link,
-            desc: offer.descriptionWebsite,
-            address: {
-                city: offer.locationCityName,
-                district: offer.locationParentPrecinctName,
-                street: offer.locationStreetName,
-                houseNumber: offer.locationBuildingnumber,
-                apartmentNumber: offer.locationApartmentnumber
-            },
-            pictures: offer.pictures,
-            tourLink: offer.tourLink,
-            videoLink: offer.videoLink,
-            price: parseInt(offer.price as string),
-            priceForm2: parseInt(offer.pricePermeter as string),
-            rent: parseInt(offer.apartmentRent as string),
-            area: parseInt(offer.areaTotal as string),
-            numOfRooms: parseInt(offer.apartmentRoomNumber),
-            constructYear: parseInt(offer.buildingYear),
-            details: {
-                Balkon: offer.additionalBalcony,
-                Liczba_lazienek: offer.apartmentBathroomNumber,
-                propFeatures: offer.tagList,
-                Wysokosc_apartamentu: offer.apartmentHeight
-            }
+          id: parseInt(offer.id),
+          category: offer.transaction && offer.transaction == 132 ? 'FOR_RENT' : 'FOR_SALE',
+          link: link,
+          desc: offer.descriptionWebsite,
+          address: {
+            city: offer.locationCityName,
+            district: offer.locationParentPrecinctName,
+            street: offer.locationStreetName,
+            houseNumber: offer.locationBuildingnumber,
+            apartmentNumber: offer.locationApartmentnumber
+          },
+          pictures: offer.pictures,
+          tourLink: offer.tourLink,
+          videoLink: offer.videoLink,
+          price: parseInt(offer.price as string),
+          priceForm2: parseInt(offer.pricePermeter as string),
+          rent: parseInt(offer.apartmentRent as string),
+          area: parseInt(offer.areaTotal as string),
+          numOfRooms: parseInt(offer.apartmentRoomNumber),
+          constructYear: parseInt(offer.buildingYear),
+          details: {
+            Balkon: offer.additionalBalcony,
+            Liczba_lazienek: offer.apartmentBathroomNumber,
+            propFeatures: offer.tagList,
+            Wysokosc_apartamentu: offer.apartmentHeight
+          }
         };
 
         return newOffer;
-    }));
+      })
+  );
 
-    offers.forEach((offer : IEstateFields) => {
-        const theSameLinkOffers = offers.filter((estioffer : IEstateFields) => { // @ts-ignore
-            if (estioffer ?. link) { // @ts-ignore
-                return estioffer ?. link === offer.link;
-            } else 
-                return false;
-            
-
-        });
-
-        if (theSameLinkOffers.length > 1) {
-            theSameLinkOffers.forEach((offer : IEstateFields, index : number) => { // @ts-ignore
-                offer.link += index > 1 ? `-${index}` : '';
-            });
-        }
+  offers.forEach((offer: IEstateFields) => {
+    const theSameLinkOffers = offers.filter((estioffer: IEstateFields) => {
+      // @ts-ignore
+      if (estioffer?.link) {
+        // @ts-ignore
+        return estioffer?.link === offer.link;
+      } else return false;
     });
 
-    offers = await Promise.all(offers.map(async (offer : IEstateFields) => { // @ts-ignore
-        offer.images = await Promise.all(offer.pictures.map(async (picture) => {
-        const index = picture.lastIndexOf('/') + 1;
-                const fileName = picture.substr(index);
-                const file = fs.createWriteStream(`public/img/offers/simple/${fileName}`, {flags: 'w'});
-                let dimensions;
+    if (theSameLinkOffers.length > 1) {
+      theSameLinkOffers.forEach((offer: IEstateFields, index: number) => {
+        // @ts-ignore
+        offer.link += index > 1 ? `-${index}` : '';
+      });
+    }
+  });
 
-                const isImage = await isImgUrl(picture);
+  offers = await Promise.all(
+    offers.map(async (offer: IEstateFields) => {
+      // @ts-ignore
+      offer.images = await Promise.all(
+        // @ts-ignore
+        offer.pictures.map(async (picture) => {
+          const index = picture.lastIndexOf('/') + 1;
+          const fileName = picture.substr(index);
+          const file = fs.createWriteStream(`public/img/offers/simple/${fileName}`, { flags: 'w' });
+          let dimensions;
 
-                if (isImage) {
-                    try {
-                        await new Promise((resolve) => {
-                            https.get(picture, async function (res) {
-                                res.pipe(file);
-                                dimensions = await sizeOf(`public/img/offers/simple/${fileName}`);
-                                const chunks_of_data: any[] = [];
+          const isImage = await isImgUrl(picture);
 
-                                res.on('data', (fragments) => {
-                                    chunks_of_data.push(fragments);
-                                });
+          if (isImage) {
+            try {
+              await new Promise((resolve) => {
+                https.get(picture, async function (res) {
+                  res.pipe(file);
+                  dimensions = await sizeOf(`public/img/offers/simple/${fileName}`);
+                  const chunks_of_data: any[] = [];
 
-                                res.on('end', () => {
-                                    const response_body = Buffer.concat(chunks_of_data);
+                  res.on('data', (fragments) => {
+                    chunks_of_data.push(fragments);
+                  });
 
-                                    resolve(response_body.toString());
-                                });
+                  res.on('end', () => {
+                    const response_body = Buffer.concat(chunks_of_data);
 
-                                res.on('error', (error) => {
-                                    console.log(error);
-                                });
-                            });
-                        });
+                    resolve(response_body.toString());
+                  });
 
-                        return { // @ts-ignore
-                            fileName: fileName.replace(`.${
-                                dimensions.type
-                            }`, ""),
-                            src: `${
-                                process.env.DOMAIN
-                            }/img/offers/simple/${fileName}`,
-                            alt: '',
-                            dimensions
-                        };
-                    } catch {return null}} else {
-                return null;
+                  res.on('error', (error) => {
+                    console.log(error);
+                  });
+                });
+              });
+
+              return {
+                // @ts-ignore
+                fileName: fileName.replace(`.${dimensions.type}`, ''),
+                src: `${process.env.DOMAIN}/img/offers/simple/${fileName}`,
+                alt: '',
+                dimensions
+              };
+            } catch {
+              return null;
             }
-        }
-    ));
-    delete offer.pictures;
-    offer.images.filter(img => img != null);
+          } else {
+            return null;
+          }
+        })
+      );
+      delete offer.pictures;
+      offer.images.filter((img) => img != null);
 
-    return offer;
-}));
+      return offer;
+    })
+  );
 
-return offers;};
+  return offers;
+};

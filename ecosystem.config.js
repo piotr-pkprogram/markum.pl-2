6ab910530c0c3246891412118b719e0e1616c978
Piotr385 <74099800+Piotr385@users.noreply.github.com>
2023-05-27 12:21:46 +0200

    Update .env variables in ecosystem files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,34 +8,34 @@ module.exports = {
       args: 'start',
       env_local: {
         APP_ENV: 'local',
-        'CONTENTFUL_API_PUBLIC_KEY': 'nrz0imrXtNkAmAhpoe6Y1nTV8cYAKZxyeQNs5hIYjiA',
-        'CONTENTFUL_API_PREVIEW_KEY': 'rjpkrg7XfxddBPD3v4Z-9z-8HT0nmrZozBGuhEvbrwo',
-        'CONTENTFUL_API_ID': 'xjfnd4eilgtd',
-        'CONTENTFUL_API_MANAGMENT_KEY': 'CFPAT-HKsmBrLGNUUSzdTheHNWnMWbeh-4kcc0TVZzwC00T7c',
-        'DOMAIN': 'http://134.122.92.71:3000',
-        'ESTICRM_ID': 6400,
-        'ESTICRM_TOKEN': '44b48b0294',
+        CONTENTFUL_API_PUBLIC_KEY: 'nrz0imrXtNkAmAhpoe6Y1nTV8cYAKZxyeQNs5hIYjiA',
+        CONTENTFUL_API_PREVIEW_KEY: 'rjpkrg7XfxddBPD3v4Z-9z-8HT0nmrZozBGuhEvbrwo',
+        CONTENTFUL_API_ID: 'xjfnd4eilgtd',
+        CONTENTFUL_API_MANAGMENT_KEY: 'CFPAT-HKsmBrLGNUUSzdTheHNWnMWbeh-4kcc0TVZzwC00T7c',
+        DOMAIN: 'http://134.122.92.71',
+        ASARI_ID: '64074',
+        ASARI_TOKEN: '0t574u4Yw3G6Jm20T26N7bRQV58bZ5U7As1P84u2'
       },
       env_development: {
         APP_ENV: 'dev',
-        'CONTENTFUL_API_PUBLIC_KEY': 'nrz0imrXtNkAmAhpoe6Y1nTV8cYAKZxyeQNs5hIYjiA',
-        'CONTENTFUL_API_PREVIEW_KEY': 'rjpkrg7XfxddBPD3v4Z-9z-8HT0nmrZozBGuhEvbrwo',
-        'CONTENTFUL_API_ID': 'xjfnd4eilgtd',
-        'CONTENTFUL_API_MANAGMENT_KEY': 'CFPAT-HKsmBrLGNUUSzdTheHNWnMWbeh-4kcc0TVZzwC00T7c',
-        'DOMAIN': 'http://134.122.92.71',
-        'ESTICRM_ID': 6400,
-        'ESTICRM_TOKEN': '44b48b0294',
+        CONTENTFUL_API_PUBLIC_KEY: 'nrz0imrXtNkAmAhpoe6Y1nTV8cYAKZxyeQNs5hIYjiA',
+        CONTENTFUL_API_PREVIEW_KEY: 'rjpkrg7XfxddBPD3v4Z-9z-8HT0nmrZozBGuhEvbrwo',
+        CONTENTFUL_API_ID: 'xjfnd4eilgtd',
+        CONTENTFUL_API_MANAGMENT_KEY: 'CFPAT-HKsmBrLGNUUSzdTheHNWnMWbeh-4kcc0TVZzwC00T7c',
+        DOMAIN: 'http://134.122.92.71',
+        ASARI_ID: '64074',
+        ASARI_TOKEN: '0t574u4Yw3G6Jm20T26N7bRQV58bZ5U7As1P84u2'
       },
       env_production: {
         APP_ENV: 'prod',
-        'CONTENTFUL_API_PUBLIC_KEY': 'nrz0imrXtNkAmAhpoe6Y1nTV8cYAKZxyeQNs5hIYjiA',
-        'CONTENTFUL_API_PREVIEW_KEY': 'rjpkrg7XfxddBPD3v4Z-9z-8HT0nmrZozBGuhEvbrwo',
-        'CONTENTFUL_API_ID': 'xjfnd4eilgtd',
-        'CONTENTFUL_API_MANAGMENT_KEY': 'CFPAT-HKsmBrLGNUUSzdTheHNWnMWbeh-4kcc0TVZzwC00T7c',
-        'DOMAIN': 'https://www.marcinkumiszczo.pl',
-        'ESTICRM_ID': 6400,
-        'ESTICRM_TOKEN': '44b48b0294',
+        CONTENTFUL_API_PUBLIC_KEY: 'nrz0imrXtNkAmAhpoe6Y1nTV8cYAKZxyeQNs5hIYjiA',
+        CONTENTFUL_API_PREVIEW_KEY: 'rjpkrg7XfxddBPD3v4Z-9z-8HT0nmrZozBGuhEvbrwo',
+        CONTENTFUL_API_ID: 'xjfnd4eilgtd',
+        CONTENTFUL_API_MANAGMENT_KEY: 'CFPAT-HKsmBrLGNUUSzdTheHNWnMWbeh-4kcc0TVZzwC00T7c',
+        DOMAIN: 'https://marcinkumiszczo.pl',
+        ASARI_ID: '64074',
+        ASARI_TOKEN: '0t574u4Yw3G6Jm20T26N7bRQV58bZ5U7As1P84u2'
       }
     }
   ]
-}
+};

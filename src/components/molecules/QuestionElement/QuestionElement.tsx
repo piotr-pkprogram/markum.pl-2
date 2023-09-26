1d892639d827e20a3529474a26922bff20444070
Piotr385 <74099800+Piotr385@users.noreply.github.com>
2023-09-26 21:26:58 +0200

    Fixed arrow rotate animation in QuestionElement component

--- src/components/molecules/QuestionElement/QuestionElement.tsx
+++ src/components/molecules/QuestionElement/QuestionElement.tsx
@@ -13,21 +13,23 @@ type Props = {
 };
 
 const QuestionElement = ({ question, className = '' }: Props) => {
-  const [isAnswerVisable, setIsAnswerVisable] = useState(false);
+  const [isAnswerVisible, setIsAnswerVisible] = useState(false);
   const wrapper = useRef<HTMLDivElement>(null);
+  const arrowEl = useRef<HTMLImageElement>(null);
 
   const switchAnswerVisibility = () => {
-    const arrow = document.querySelector('#arrow-down');
+    const questArrow = arrowEl.current as HTMLImageElement;
     const questWrapper = wrapper.current as HTMLDivElement;
+    console.log(questArrow);
 
-    if (isAnswerVisable) {
-      arrow?.classList.remove('rotate-90');
+    if (isAnswerVisible) {
+      questArrow?.classList.remove('rotate-90');
       questWrapper.classList.remove('!pb-3');
-      setIsAnswerVisable(false);
+      setIsAnswerVisible(false);
     } else {
-      arrow?.classList.add('rotate-90');
+      questArrow?.classList.add('rotate-90');
       questWrapper.classList.add('!pb-3');
-      setIsAnswerVisable(true);
+      setIsAnswerVisible(true);
     }
   };
 
@@ -36,16 +38,17 @@ const QuestionElement = ({ question, className = '' }: Props) => {
       id={deletePolishChars(question.question).toLowerCase().replace("?", "")}
       className={`${styles.wrapper} ${className}`}
       ref={wrapper}
+      onClick={switchAnswerVisibility}
     >
-      <Image
+      <img
         id="arrow-down"
         className="transition-transform cursor-pointer"
-        src={arrow}
+        src={'/img/down-arrow.svg'}
         alt=""
-        onClick={switchAnswerVisibility}
+        ref={arrowEl}
       />
       <h3 className="font-semibold text-2xl row-start-1">{question.question}</h3>
-      {isAnswerVisable ? (
+      {isAnswerVisible ? (
         <p id="description" className="row-start-2 pb-7 pr-5">
           {parseHTML(question.answer.replaceAll("\n", "<br>"))}
         </p>

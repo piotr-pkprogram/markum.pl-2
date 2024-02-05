60e08b2cb38503727c74dd862bdf2dc09600b66e
Piotr385 <74099800+Piotr385@users.noreply.github.com>
2024-02-05 15:59:46 +0100

    Add FB Conversion Api Tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,21 +7,35 @@ import { CookiesProvider } from 'react-cookie';
 import Head from 'next/head';
 // @ts-ignore
 import loadable from '@loadable/component';
+import { FBPixelScript, FBPixelProvider } from '@rivercode/facebook-conversion-api-nextjs/components';
+import { fbEvent } from '@rivercode/facebook-conversion-api-nextjs';
+import { useEffect } from 'react';
 
 const Layout = loadable(() => import('../components/templates/Layout/Layout'));
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+
+  useEffect(() => {
+    fbEvent({
+      eventName: 'ViewContent', // ViewContent, AddToCart, InitiateCheckout, Purchase etc.
+      enableStandardPixel: false // default false (Require Facebook Pixel to be loaded, see step 2)
+    });
+  }, []);
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       </Head>
       <CookiesProvider>
-        <Provider store={store}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </Provider>
+        <FBPixelScript />
+        <FBPixelProvider>
+          <Provider store={store}>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </Provider>
+        </FBPixelProvider>
       </CookiesProvider>
     </>
   );
